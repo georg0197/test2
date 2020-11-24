4e815addb2b09cce754e2d9677877077a8854330
Baha-sagandykova <74654109+Baha-sagandykova@users.noreply.github.com>
2020-11-24 22:21:19 +0600

    Update main_dice.py

--- main_dice.py
+++ main_dice.py
@@ -29,7 +29,7 @@ def draw_dice(*args):
         'dot3': lambda x, y, r: c.create_oval(x, y + 8, x + r, (y + 8) + r, fill='red'),
         'dot4': lambda x, y, r: c.create_oval(x + 8, (y + 8), (x + 8) + r, (y + 8) + r, fill='purple'),
         'dot5': lambda x, y, r: c.create_oval(x + 16, (y + 8), (x + 16) + r, (y + 8) + r, fill='brown'),
-        'dot6': lambda x, y, r: c.create_oval(x, y + 16, x + r, (y + 16) + r, fill='black'),
+        'dot6': lambda x, y, r: c.create_oval(x, y + 16, x + r, (y + 16) + r, fill='pink'),
         'dot9': lambda x, y, r: c.create_oval(x + 16, y + 16, (x + 16) + r, (y + 16) + r, fill='grey')
     }
 
