a2d0f3d73bec3f20fc3162a74975855c0c1bb20a
georg0197 <74650254+georg0197@users.noreply.github.com>
2020-11-24 21:42:53 +0600

    commit 2
    Удаление всех комментариев в коде

--- main_dice.py
+++ main_dice.py
@@ -5,23 +5,23 @@ from random import randint
 def create_dice():
     
     dice = list()
-    dice.append(draw_dice('dot0'))  # empty
-    dice.append(draw_dice('dot4'))  # center dot --> 1
-    dice.append(draw_dice('dot3', 'dot5'))  # dice head 2
-    dice.append(draw_dice('dot2', 'dot4', 'dot6'))  # dice head 3
-    dice.append(draw_dice('dot1', 'dot2', 'dot6', 'dot9'))  # dice head 4
-    dice.append(draw_dice('dot1', 'dot2', 'dot4', 'dot6', 'dot9'))  # dice head 5
-    dice.append(draw_dice('dot1', 'dot2', 'dot3', 'dot5', 'dot6', 'dot9'))  # dice head 6
+    dice.append(draw_dice('dot0')) 
+    dice.append(draw_dice('dot4'))  
+    dice.append(draw_dice('dot3', 'dot5'))  
+    dice.append(draw_dice('dot2', 'dot4', 'dot6')) 
+    dice.append(draw_dice('dot1', 'dot2', 'dot6', 'dot9'))  
+    dice.append(draw_dice('dot1', 'dot2', 'dot4', 'dot6', 'dot9')) 
+    dice.append(draw_dice('dot1', 'dot2', 'dot3', 'dot5', 'dot6', 'dot9')) 
     return dice
 
 
 def draw_dice(*args):
     
-    w, h = 250, 250 # sets width and height
-    x, y, r = 2, 2, 5 # sets x, y, and radius
-    c = tk.Canvas(root, width=w, height=h, bg='white') # creates canvas c
+    w, h = 250, 250 
+    x, y, r = 2, 2, 5
+    c = tk.Canvas(root, width=w, height=h, bg='white') 
 
-    #Dictionary containing lambda functions to draw dots on canvas c
+    
     dots = {
         'dot0': lambda x, y, r: c,
         'dot1': lambda x, y, r: c.create_oval(x, y, x + r, y + r, fill='black'),
@@ -34,43 +34,43 @@ def draw_dice(*args):
     }
 
     for arg in args:
-        dots.get(arg)(x, y, r) # Gets the dictionary keys while passing in x, y, and r values
+        dots.get(arg)(x, y, r) 
 
     return c
 
 
 def click():
    
-    t = 100 # start with a time delay of 100 ms and increase it as the dice rolls
-    stop = randint(13, 18) # chooses random number between 13 - 17
+    t = 100 
+    stop = randint(13, 18) 
     for x in range(stop):
-        dice_index = x % 6 + 1 # gets the randomly selected dice head by modulo
+        dice_index = x % 6 + 1 
         dice_list[dice_index].grid(row=1, column=0, columnspan=3)
         root.update()
         if x == stop - 1:
-            # set text to the selected result
+            
             text.set(str(x % 6 + 1))
             break
-        root.after(t, dice_list[dice_index].grid_forget()) # forgets the grid and restarts
+        root.after(t, dice_list[dice_index].grid_forget()) 
         t += 25
 
 
-# create the window form
+
 root = tk.Tk()
 root.title("Игра Кости ")
 
-# StringVar() updates result label automatically
+
 text = tk.StringVar()
 
-# set initial value of text
+
 text.set("")
 
-# create the result label
+
 result = tk.Label(root, textvariable=text, fg='black')
 result.grid(row=3, column=0, columnspan=3)
 dice_list = create_dice()
 
-# start with an empty canvas
+
 dice_list[0].grid(row=1, column=0, columnspan=3)
 button1 = tk.Button(root, text="Бросить кости", command=click)
 button1.grid(row=2, column=0, padx=3, pady=3)
@@ -83,6 +83,6 @@ filemenu.add_command(label='Доп. информация', command=info)
 menu.add_cascade(label='Файл', menu=filemenu)
 menu.add_command(label='Справка', command=f1)
 root.config(menu=menu) 
-# start of program event loop
+
 root.mainloop()
 
